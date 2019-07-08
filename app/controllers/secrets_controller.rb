677b83fb924da619cc82bcd4c356f37acd0ceaa2
Alexander Riccio <test35965+learn@gmail.com>
2019-07-08 19:39:25 -0400

    solve lab

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,6 @@
+class SecretsController < ApplicationController
+    before_action :authorized?
+    def show
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :secrets, only: [:show]
+  get '/sessions/new', to: 'sessions#new', as: 'login'
+  post '/sessions', to: 'sessions#create'
+  post '/sessions/destroy', to: 'sessions#destroy'
+  get '/secrets', to: 'secrets#show'
 end
